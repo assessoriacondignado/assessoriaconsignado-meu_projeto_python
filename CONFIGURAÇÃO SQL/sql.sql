80fce27753d546c04a29eca41574329787630246
assessoriacondignado <atendimento@assessoriaconsignado.com>
2025-12-29 07:45:17 -0300

    atualização site

--- CONFIGURAÇÃO SQL/sql.sql
+++ CONFIGURAÇÃO SQL/sql.sql
@@ -1,2 +1,7 @@
--- Move a tabela de contratos CLT para o schema correto
-ALTER TABLE admin.pf_contratos_clt SET SCHEMA banco_pf;
+-- 1. Remove a restrição que impede a mudança (Chave Estrangeira)
+ALTER TABLE banco_pf.pf_dados 
+DROP CONSTRAINT IF EXISTS pf_dados_importacao_id_fkey;
+
+-- 2. Agora altera a coluna para TEXTO para aceitar listas (ex: "10, 12, 15")
+ALTER TABLE banco_pf.pf_dados 
+ALTER COLUMN importacao_id TYPE VARCHAR(255);
