938cfc5f79aef42bb9482cacf0b4be57ba993862
assessoriacondignado <atendimento@assessoriaconsignado.com>
2026-01-02 09:46:15 -0300

    correção tabela

--- CONFIGURAÇÃO SQL/sql.sql
+++ CONFIGURAÇÃO SQL/sql.sql
@@ -1,3 +1,3 @@
-ALTER TABLE cliente.cliente_carteira_lista
-ADD COLUMN IF NOT EXISTS cpf_usuario VARCHAR(20),
-ADD COLUMN IF NOT EXISTS nome_usuario VARCHAR(255);
+UPDATE cliente.carteiras_config 
+SET origem_custo = 'WEB USUÁRIO' 
+WHERE origem_custo = 'USUÁRIO WEB';
