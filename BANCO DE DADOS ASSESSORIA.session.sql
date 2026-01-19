a6b0affba0b998a53fd9c3c6653e859bc723991f
assessoriacondignado <atendimento@assessoriaconsignado.com>
2026-01-19 15:29:08 -0300

    asql

--- BANCO DE DADOS ASSESSORIA.session.sql
+++ BANCO DE DADOS ASSESSORIA.session.sql
@@ -1,6 +1,2 @@
--- RODAR NO SEU BANCO DE DADOS
-ALTER TABLE sistema_consulta.sistema_consulta_importacao 
-ADD COLUMN IF NOT EXISTS id_usuario TEXT;
-
-ALTER TABLE sistema_consulta.sistema_consulta_importacao 
-ADD COLUMN IF NOT EXISTS nome_usuario TEXT;
+ALTER TABLE sistema_consulta."SISTEMA_IMPORTACAO_TIPO"
+ADD COLUMN tabela_importacao_staging TEXT;
