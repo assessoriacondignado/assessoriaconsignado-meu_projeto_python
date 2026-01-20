4b7af5b174a8f4b363a4de13edbc9f9ee41c316a
assessoriacondignado <atendimento@assessoriaconsignado.com>
2026-01-20 13:25:20 -0300

    correção

--- BANCO DE DADOS ASSESSORIA.session.sql
+++ BANCO DE DADOS ASSESSORIA.session.sql
@@ -1,7 +1,2 @@
--- 1. Adicionar CPF na tabela de Staging (Temporária)
-ALTER TABLE sistema_consulta.importacao_staging_convenio_clt 
-ADD COLUMN IF NOT EXISTS cpf VARCHAR(20);
-
--- 2. Adicionar CPF na tabela Final (Dados CTT)
-ALTER TABLE sistema_consulta.sistema_consulta_dados_ctt 
-ADD COLUMN IF NOT EXISTS cpf VARCHAR(20);
+ALTER TABLE sistema_consulta.sistema_consulta_dados_ctt
+RENAME TO sistema_consulta_dados_clt;
