8ac71c72897b3ea21f90ded09218b90e108648a9
assessoriacondignado <atendimento@assessoriaconsignado.com>
2026-01-23 06:17:24 -0300

    calculo

--- BANCO DE DADOS ASSESSORIA.session.sql
+++ BANCO DE DADOS ASSESSORIA.session.sql
@@ -1,11 +1,6 @@
--- 1. Apagar os telefones duplicados (mantendo apenas o mais recente ou o primeiro)
-DELETE FROM sistema_consulta.sistema_consulta_dados_cadastrais_telefone a
-USING sistema_consulta.sistema_consulta_dados_cadastrais_telefone b
-WHERE a.id < b.id
-  AND a.cpf = b.cpf
-  AND a.telefone = b.telefone;
-
--- 2. Criar a "Trava" de Unicidade
--- Isso impede que o banco aceite CPF + Telefone repetidos
-ALTER TABLE sistema_consulta.sistema_consulta_dados_cadastrais_telefone
-ADD CONSTRAINT unique_cpf_telefone UNIQUE (cpf, telefone);
+-- Sincroniza a sequência da tabela de TELEFONES
+SELECT setval(
+    pg_get_serial_sequence('sistema_consulta.sistema_consulta_dados_cadastrais_telefone', 'id'),
+    COALESCE((SELECT MAX(id) FROM sistema_consulta.sistema_consulta_dados_cadastrais_telefone) + 1, 1),
+    false
+);
